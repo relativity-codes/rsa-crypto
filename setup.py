451a033e9dc5d21e681e4706adc402ebad81ea8f
relativity-codes <ukweheverest@gmail.com>
2024-02-20 18:37:46 +0100

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rsa-crypto-python',
-    version='1.0.1',
+    version='1.0.7',
     packages=find_packages(),
     install_requires=[
         'cryptography',
